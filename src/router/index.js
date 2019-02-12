f383b6545246314b7e005d037979648ac044d2ca
hangole1999 <hangole1999@gmail.com>
2019-02-12 13:56:46 +0900

    [Test] Fix router for test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HelloWorld2 from '@/components/HelloWorld2'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '*',
+      name: 'HelloWorld2',
+      component: HelloWorld2
     }
   ]
 })
